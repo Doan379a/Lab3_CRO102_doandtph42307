036581f2cfacd313ffe898a67fe5c18688c28c4b
Doan379a <doan379a@gmail.com>
2024-03-18 21:27:18 +0700

    doando

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Bai3 from './Lab3/bai3';
+import Bai2 from './Lab3/bai2';
+import Bai1 from './Lab3/bai1';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Bai3);
